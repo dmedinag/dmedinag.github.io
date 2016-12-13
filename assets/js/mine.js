3e6498743e9912d09a98d4cd1c330583d3e234a1
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-12-12 20:01:05 -0500

    webpage init

---
+++ assets/js/mine.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $('.esp').hide();
+});
+
+$("#switchs").click(function(){
+    $('.eng').hide();
+    $('.esp').show();
+});
+
+$("#switche").click(function(){
+    $('.esp').hide();
+    $('.eng').show();
+});
